04b0cf5ce160050fc692fea085f4ed19d1309ab3
Song <quanpengsong@gmail.com>
2021-08-08 11:33:23 +0800

    implement the Style Transformer

--- models/build.py
+++ models/build.py
@@ -2,26 +2,26 @@ import copy
 from munch import Munch
 from models.generator import Generator
 from models.discriminator import Discriminator
-from models.mapping_network import MappingNetwork
+from models.style_transformer import StyleTransformer
 from models.style_encoder import StyleEncoder
 from models.wing import FAN
 
 
 def build_model(args):
     generator = Generator(args)
-    mapping_network = MappingNetwork(args)
+    style_transformer = StyleTransformer(args)
     style_encoder = StyleEncoder(args)
     discriminator = Discriminator(args)
     generator_ema = copy.deepcopy(generator)
-    mapping_network_ema = copy.deepcopy(mapping_network)
+    style_transformer_ema = copy.deepcopy(style_transformer)
     style_encoder_ema = copy.deepcopy(style_encoder)
 
     nets = Munch(generator=generator,
-                 mapping_network=mapping_network,
+                 style_transformer=style_transformer,
                  style_encoder=style_encoder,
                  discriminator=discriminator)
     nets_ema = Munch(generator=generator_ema,
-                     mapping_network=mapping_network_ema,
+                     style_transformer=style_transformer_ema,
                      style_encoder=style_encoder_ema)
 
     if args.w_hpf > 0:
---
+++ models/style_transformer.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+
+class StyleTransformer(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        style_dim, num_domains = args.style_dim, args.num_domains
+        layers = []
+        layers += [nn.Linear(style_dim, style_dim)]
+        layers += [nn.ReLU()]
+        for _ in range(3):
+            layers += [nn.Linear(style_dim, style_dim)]
+            layers += [nn.ReLU()]
+        self.decomposer = nn.Sequential(*layers)
+
+        self.domain_transformer = nn.ModuleList()
+        for _ in range(num_domains):
+            self.domain_transformer += [nn.Sequential(nn.Linear(style_dim, style_dim), nn.ReLU(),
+                                                      nn.Linear(style_dim, style_dim), nn.ReLU(),
+                                                      nn.Linear(style_dim, style_dim), nn.ReLU(),
+                                                      nn.Linear(style_dim, style_dim))]
+
+    def forward(self, s, y):
+        s_unrelated = self.decomposer(s)
+        s_related = s - s_unrelated
+        out = []
+        for layer in self.domain_transformer:
+            out += [layer(s_related)]
+        out = torch.stack(out, dim=1)  # (batch, num_domains, style_dim)
+        idx = torch.LongTensor(range(y.size(0))).to(y.device)
+        s_related = out[idx, y]  # (batch, style_dim)
+        s = s_related + s_unrelated
+        return s
